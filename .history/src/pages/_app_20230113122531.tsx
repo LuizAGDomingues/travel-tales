c426b06ffafd89a66c58dac62e870ff66205da46
LuizAGDomingues <luizarthur1234@gmail.com>
2023-01-27 19:35:08 -0300

    Bug fix

--- .history/src/pages/_app_20230113122531.tsx
+++ .history/src/pages/_app_20230113122531.tsx
@@ -1,16 +1,16 @@
-import '@/styles/globals.css';
-import { Poppins } from '@next/font/google';
-import type { AppProps } from 'next/app';
+import '@/styles/globals.css'
+import { Poppins } from '@next/font/google'
+import type { AppProps } from 'next/app'
 
 const poppins = Poppins({
   subsets: ['latin'],
   weight: '300'
-});
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={poppins.className}>
       <Component {...pageProps} />
     </main>
-  );
+  )
 }
